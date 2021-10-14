b096e99b6d5f03fa586545bf131dd2ad631af570
Niladrib800 <92511374+Niladrib800@users.noreply.github.com>
2021-10-14 14:02:34 +0530

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter two numbers:");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    c = a+b;
+    printf("sum = %d",c);
+    return 0;
+}
